64a6aad69deca5295675cd49ea77578d9cc89f81
teppei667 <teptympei.sasstjgada.666@gmail.com>
2021-11-30 16:06:48 +0900

    Add react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+// components
+import { Restaurants } from './containers/Restaurants.jsx';
+import { Foods } from './containers/Foods.jsx';
+import { Orders } from './containers/Orders.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Routes>
+        //店舗一覧ページ
+        <Route
+          exact
+          path="/restaurants"
+          element={<Restaurants />} />  
+        
+        <Route
+          exact
+          path="/orders"
+          element={<Orders />} /> 
+        <Route
+          exact
+          path="/restaurants/:restaurantsId/foods"
+          render={({ match }) =>
+            <Foods
+              match={match} />
+            }
+        />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
