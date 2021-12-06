6493eaa857c78cc63c98ed35c5829e79256dec3a
teppei667 <teptympei.sasstjgada.666@gmail.com>
2021-12-06 14:07:02 +0900

    Add food order dialog

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useReducer } from "react";
+import React, { Fragment, useEffect, useReducer, useState } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom"
 
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom"
 import { LocalMallIcon } from "../components/Icons";
 import { FoodWrapper } from "../components/FoodWrapper";
 import Skeleton from '@material-ui/lab/Skeleton';
+import { FoodOrderDialog } from "../components/FoodOrderDialog";
 
 //reducers
 import {
@@ -59,6 +60,14 @@ export const Foods = ({ match }) => {
 
   const [foodsState, dispatch] = useReducer(foodsReducer, foodsInitialState);
 
+  const initialState = {
+    isOpenOrderDialog: false,
+    selectedFood: null,
+    selectedFoodCount: 1,
+  }
+
+  const [state, setState] = useState(initialState);
+
   useEffect(() => {
     dispatch({ type: foodsActionTypes.FETCHING })
     fetchFoods(match.params.restaurantsId)
@@ -101,13 +110,31 @@ export const Foods = ({ match }) => {
               <ItemWrapper key={food.id}>
                 <FoodWrapper
                   food={food}
-                  onClickFoodWrapper={(food) => console.log(food)}
+                  //フードアイテムクリック時にsetStateする
+                  onClickFoodWrapper={
+                    (food) => setState({
+                      ...state,
+                      isOpenOrderDialog: true,
+                      selectedFood: food,
+                    })
+                  }
                   imageUrl={FoodImage}
                 />
               </ItemWrapper>
             )
         }
       </FoodsList>
+      {
+        state.isOpenOrderDialog &&
+          <FoodOrderDialog
+            food={state.selectedFood}
+            isOpen={state.isOpenOrderDialog}
+            onClose={() => setState({
+              ...state,
+              isOpenOrderDialog: false,
+            })}
+          />
+      }
     </Fragment>
   )
 }
